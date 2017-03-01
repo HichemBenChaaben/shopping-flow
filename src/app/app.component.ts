28ce58b993e05a7a465b2451452ef65ca045db41
hichem <hichem@Samers-MacBook-Pro.local>
2017-03-02 00:04:51 +0400

    Added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: this.module,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  products = [
+    {
+      'name': 'p1209123j',
+      'price': '230euro',
+      'discount': 20
+    },
+    {
+      'name': 'kqwjdpjqwd9812ep1209123j',
+      'price': '230euro',
+      'discount': 20
+    },
+    {
+      'name': 'pp1209123j123kk',
+      'price': '230euro',
+      'discount': 20
+    },
+    {
+      'name': 'p1209123j19u2312e012e',
+      'price': '230euro',
+      'discount': 20
+    }
+  ];
 }
